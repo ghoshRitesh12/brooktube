441db9ebeea58416bb1b469092908e2e91849f63
Ritesh Ghosh <ghoshritesh55@gmail.com>
2024-04-27 00:49:45 +0530

    feat: add constants for YouTube Music API endpoints and headers

---
+++ utils/constants.go
@@ -0,0 +1,29 @@
+package utils
+
+const (
+	HOST                    string = "https://music.youtube.com"
+	BROWSE_PATH             string = "/youtubei/v1/browse"
+	PLAYER_PATH             string = "/youtubei/v1/player"
+	NEXT_PATH               string = "/youtubei/v1/next"
+	QUEUE_PATH              string = "/youtubei/v1/music/get_queue"
+	SEARCH_PATH             string = "/youtubei/v1/search"
+	SEARCH_SUGGESTIONS_PATH string = "/youtubei/v1/music/get_search_suggestions"
+)
+
+const (
+	USER_AGENT_HEADER string = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36"
+)
+
+const (
+	HL             string = "en"
+	PLATFORM       string = "DESKTOP"
+	TIME_ZONE      string = "Europe/Berlin"
+	CLIENT_NAME    string = "WEB_REMIX"
+	CLIENT_VERSION string = "1.20240422.01.01"
+)
+
+const (
+	GOOG_FIELD_MASK string = "*"
+	GOOG_VISITOR_ID string = "CgtKc19vbFBzMEpYcyjfz6-xBjIKCgJJThIEGgAgJA%3D%3D"
+	GOOG_API_KEY    string = "AIzaSyAO_FJ2SlqU8Q4STEHLGCilw_Y9_11qcW8"
+)
