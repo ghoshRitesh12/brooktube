a4ee885ca5cb61d462792e8a485bcac9f9f154f6
Ritesh Ghosh <ghoshritesh55@gmail.com>
2024-04-29 23:58:58 +0530

    feat: add ParseSearchContent util fn

---
+++ utils/parseSearchContent.go
@@ -0,0 +1,139 @@
+package utils
+
+import (
+	"strings"
+
+	"github.com/ghoshRitesh12/yt_music/types/search"
+)
+
+func ParseSearchContent(category search.SearchCategory, shelfContents []search.RespSectionContent) search.ResultContent {
+	resultContent := search.ResultContent{}
+
+	switch category {
+	case search.SONG_SEARCH_KEY, search.VIDEO_SEARCH_KEY:
+		for _, content := range shelfContents {
+			separator := " • "
+			var otherInfoBuilder strings.Builder
+			songOrVideo := search.SongOrVideo{
+				VideoId: content.MusicResponsiveListItemRenderer.PlaylistItemData.VideoId,
+			}
+
+			for i, flexColumn := range content.MusicResponsiveListItemRenderer.FlexColumns {
+				if i == 0 {
+					songOrVideo.Name = ParseYtTextField(ParseYtTextParams{
+						FlexColumnRuns: flexColumn.MusicResponsiveListItemFlexColumnRenderer.Text.Runs,
+					})
+				} else {
+					otherInfoBuilder.WriteString(
+						ParseYtTextField(ParseYtTextParams{
+							FlexColumnRuns: flexColumn.MusicResponsiveListItemFlexColumnRenderer.Text.Runs,
+						}) + separator,
+					)
+				}
+			}
+
+			otherInfo, _ := strings.CutSuffix(otherInfoBuilder.String(), separator)
+			songOrVideo.OtherInfo = otherInfo
+
+			resultContent.SongOrVideos = append(resultContent.SongOrVideos, songOrVideo)
+		}
+
+	case search.ARTIST_SEARCH_KEY:
+		for _, content := range shelfContents {
+			artist := search.Artist{}
+			var otherInfoBuilder strings.Builder
+
+			for i, flexColumn := range content.MusicResponsiveListItemRenderer.FlexColumns {
+				if i == 0 {
+					artist.Name = ParseYtTextField(ParseYtTextParams{
+						FlexColumnRuns: flexColumn.MusicResponsiveListItemFlexColumnRenderer.Text.Runs,
+					})
+				} else {
+					otherInfoBuilder.WriteString(ParseYtTextField(ParseYtTextParams{
+						FlexColumnRuns: flexColumn.MusicResponsiveListItemFlexColumnRenderer.Text.Runs,
+					}))
+				}
+			}
+
+			artist.OtherInfo = otherInfoBuilder.String()
+
+			browseEndpoint := content.MusicResponsiveListItemRenderer.
+				NavigationEndpoint.BrowseEndpoint
+
+			if strings.Contains(
+				browseEndpoint.BrowseEndpointContextSupportedConfigs.
+					BrowseEndpointContextMusicConfig.PageType,
+				"ARTIST",
+			) {
+				artist.ArtistChannelId = browseEndpoint.BrowseID
+			}
+
+			resultContent.Artists = append(resultContent.Artists, artist)
+		}
+
+	case search.ALBUM_SEARCH_KEY:
+		for _, content := range shelfContents {
+			album := search.Album{}
+			var otherInfoBuilder strings.Builder
+
+			for i, flexColumn := range content.MusicResponsiveListItemRenderer.FlexColumns {
+				if i == 0 {
+					album.Name = ParseYtTextField(ParseYtTextParams{
+						FlexColumnRuns: flexColumn.MusicResponsiveListItemFlexColumnRenderer.Text.Runs,
+					})
+				} else {
+					otherInfoBuilder.WriteString(ParseYtTextField(ParseYtTextParams{
+						FlexColumnRuns: flexColumn.MusicResponsiveListItemFlexColumnRenderer.Text.Runs,
+					}))
+				}
+			}
+
+			album.OtherInfo = otherInfoBuilder.String()
+			resultContent.Albums = append(resultContent.Albums, album)
+		}
+
+	case search.COMMUNITY_PLAYLIST_SEARCH_KEY:
+		for _, content := range shelfContents {
+			communityPlaylist := search.CommunityPlaylist{}
+			var otherInfoBuilder strings.Builder
+
+			for i, flexColumn := range content.MusicResponsiveListItemRenderer.FlexColumns {
+				if i == 0 {
+					communityPlaylist.Name = ParseYtTextField(ParseYtTextParams{
+						FlexColumnRuns: flexColumn.MusicResponsiveListItemFlexColumnRenderer.Text.Runs,
+					})
+				} else {
+					otherInfoBuilder.WriteString(ParseYtTextField(ParseYtTextParams{
+						FlexColumnRuns: flexColumn.MusicResponsiveListItemFlexColumnRenderer.Text.Runs,
+					}))
+				}
+			}
+
+			communityPlaylist.OtherInfo = otherInfoBuilder.String()
+			resultContent.CommunityPlaylists = append(resultContent.CommunityPlaylists, communityPlaylist)
+		}
+
+	case search.FEATURED_PLAYLIST_SEARCH_KEY:
+		for _, content := range shelfContents {
+			featuredPlaylist := search.FeaturedPlaylist{}
+			var otherInfoBuilder strings.Builder
+
+			for i, flexColumn := range content.MusicResponsiveListItemRenderer.FlexColumns {
+				if i == 0 {
+					featuredPlaylist.Name = ParseYtTextField(ParseYtTextParams{
+						FlexColumnRuns: flexColumn.MusicResponsiveListItemFlexColumnRenderer.Text.Runs,
+					})
+				} else {
+					otherInfoBuilder.WriteString(ParseYtTextField(ParseYtTextParams{
+						FlexColumnRuns: flexColumn.MusicResponsiveListItemFlexColumnRenderer.Text.Runs,
+					}))
+				}
+			}
+
+			featuredPlaylist.OtherInfo = otherInfoBuilder.String()
+			resultContent.FeaturedPlaylists = append(resultContent.FeaturedPlaylists, featuredPlaylist)
+		}
+	}
+
+	return resultContent
+}
