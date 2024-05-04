c76590f035722ca83ea17d0990ef9bbb374af3aa
Ritesh Ghosh <ghoshritesh55@gmail.com>
2024-05-04 20:55:03 +0530

    refactor: search suggestions parser and import in parsers package

--- parsers/search_suggestions.go
+++ parsers/search_suggestions.go
@@ -1,29 +1,10 @@
 package parsers
 
 import (
-	"fmt"
-
-	"github.com/ghoshRitesh12/yt_music/types"
-	"github.com/ghoshRitesh12/yt_music/utils"
+	models "github.com/ghoshRitesh12/brooktube/models"
+	"github.com/ghoshRitesh12/brooktube/requests"
 )
 
-// Performs a POST to fetch search suggestions
-func (p *YtParser) GetSearchSuggestions(query string) (types.SearchSuggestions, error) {
-	url := fmt.Sprintf("%s%s?prettyPrint=false", utils.HOST, utils.SEARCH_SUGGESTIONS_PATH)
-
-	method := "POST"
-	body := map[string]any{
-		"input": query,
-	}
-	headers := map[string]string{
-		"X-Goog-Visitor-Id":        utils.GOOG_VISITOR_ID,
-		"X-Youtube-Client-Version": utils.CLIENT_VERSION,
-	}
-
-	data, err := fetch[types.SearchSuggestions](method, url, body, headers)
-	if err != nil {
-		return data, err
-	}
-
-	return data, nil
+func (p *YtMusicParser) GetSearchSuggestions(query string) (models.SearchSuggestions, error) {
+	return requests.FetchSearchSuggestions(query)
 }
