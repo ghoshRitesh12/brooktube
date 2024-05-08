662735c6c256be9d27ff8cdca250e7808a1af46e
Ritesh Ghosh <ghoshritesh55@gmail.com>
2024-05-08 21:25:47 +0530

    test: update parsing logic for `SongOrVideo` contents

--- parsers/search_test.go
+++ parsers/search_test.go
@@ -1,9 +1,9 @@
 package parsers_test
 
 import (
-	"fmt"
 	"testing"
 
+	"github.com/davecgh/go-spew/spew"
 	"github.com/ghoshRitesh12/brooktube"
 	"github.com/ghoshRitesh12/brooktube/models/search"
 	"github.com/ghoshRitesh12/brooktube/parsers"
@@ -15,12 +15,17 @@ func TestGetSearchResults(t *testing.T) {
 	d, err := parser.GetSearchResults(
 		"black clover",
 		parsers.SearchParserParams{
-			Category:          search.SONG_SEARCH_KEY,
-			ContinuationToken: "ErEDEgxibGFjayBjbG92ZXIaoANFZ1dLQVFJSUFVZ29haEFRQ2hBREVBUVFDUkFRRUFVUUVSQVZnZ0VMVTFWT1NITldkRVIwVEhlQ0FRdFlaMHRKTVRaZllXTm9WWUlCQzJwcFQxVmFaVzFhYTFNNGdnRUxMVGx6TUV3dGNVWndSRGlDQVF0YVdVWlJTRFJyYldJNFZZSUJDMU5MWTBNM2RWVlNWRGRCZ2dFTGRuVnJYM1YzZVdVd1MxV0NBUXROZFdWMmJXWmFOVkZuUVlJQkMxQnVSWGRvVms1TVJXRmpnZ0VMZDJsclgwaHNXVzUwU1VHQ0FRdEhaVEowYlZOSGRuSlJhNElCQzNvM1h6aFFkV3BZYlhacmdnRUxPWFpYVG1GMVlWcEJaMmVDQVFzeE56QnpZMlZQVjFkWVk0SUJDM0ZVUzIxcmVIcFNNMGhyZ2dFTFYyZFFWMTlsYjFad05FMkNBUXN3ZGtaRVVHNXJiMWwxVVlJQkMxQTFXa0pKVTI4eVEyRkZnZ0VMT1RWNGJrbzRVVXhoTm5PQ0FRdFRjekJHTUhNelVDMXJNQSUzRCUzRBjx6tAu",
+			Category: search.COMMUNITY_PLAYLIST_SEARCH_KEY,
+			// ContinuationToken: "EqkDEgxibGFjayBjbG92ZXIamANFZ1dLQVFJSUFVZ1VhZ29RQ1JBRkVBb1FBeEFFZ2dFTE5qZGZaVkppVWpoWWFsV0NBUXRIUkd0MGVYQnpWbVk1V1lJQkMwVkZOak5sTWxKS2JIQmpnZ0VMVDFweWFEWlZORU41UTNlQ0FRdEZOMVZ1ZFc5dk1IWklRWUlCQzFKZmIzcEllbEJEWWxZMGdnRUxaRk56VG1neFp5MHlORldDQVF0NFFXdFRYeTFHVkVoSmM0SUJDM1pPWjFoWFUxQlJkMncwZ2dFTGFIVmpTVlZqZG1KQ2JVbUNBUXQ1U1d4Q1lqRktkbXBpYTRJQkMwMTFPSHBIUzJOWlgwTnJnZ0VMWDJRdE9FZ3RPRXhxWTFXQ0FRdHpYMkoxT1ZFd01ISTBRWUlCQzFoblMwa3hObDloWTJoVmdnRUxTMGRhU0UxTWRHTlJNVldDQVF0VFZVNUljMVowUkhSTWQ0SUJDekJNTjB0V1RYSXlZVkZuZ2dFTExUbHpNRXd0Y1Vad1JEaUNBUXRNVlRST2FVcFRRa05JUVElM0QlM0QY8erQLg%3D%3D",
 		},
 	)
 
-	fmt.Printf("SEARCH DATA:  %+v\n\n", d)
+	for _, cmPlaylist := range d.Content.CommunityPlaylists {
+		spew.Dump(cmPlaylist)
+	}
+
+	spew.Dump(d.ContinuationToken)
+
 	if err != nil {
 		t.Fatal(err)
 	}
