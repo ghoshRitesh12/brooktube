0c7b589b025a79cacc46b61d6e30e8534dfa9d97
Ritesh Ghosh <ghoshritesh55@gmail.com>
2024-06-01 18:55:29 +0530

    feat: add navigation endpoints structs and methods

---
+++ models/common.go
@@ -0,0 +1,125 @@
+package models
+
+import "strings"
+
+type BasicRuns []struct {
+	Text string
+}
+
+func (runs BasicRuns) GetTextField() string {
+	var str strings.Builder
+
+	if len(runs) > 0 {
+		for _, val := range runs {
+			str.WriteString(val.Text)
+		}
+		return str.String()
+	}
+
+	return str.String()
+}
+
+type NavigationEndpointRuns []struct {
+	Text string
+
+	NavigationEndpoint struct {
+		BrowseEndpoint struct {
+			BrowseID string `json:"browseId,omitempty"`
+
+			BrowseEndpointContextSupportedConfigs struct {
+				BrowseEndpointContextMusicConfig struct {
+					PageType string `json:"pageType,omitempty"`
+				} `json:"browseEndpointContextMusicConfig,omitempty"`
+			} `json:"browseEndpointContextSupportedConfigs,omitempty"`
+		} `json:"browseEndpoint,omitempty"`
+	} `json:"navigationEndpoint,omitempty"`
+}
+
+func (runs *NavigationEndpointRuns) GetTextField() string {
+	var str strings.Builder
+
+	if len((*runs)) > 0 {
+		for _, val := range *runs {
+			str.WriteString(val.Text)
+		}
+		return str.String()
+	}
+
+	return str.String()
+}
+
+func (runs *NavigationEndpointRuns) GetFirstNavData() (pageType, browseId string) {
+	if len((*runs)) == 0 {
+		return
+	}
+
+	run := (*runs)[0]
+	browseId = run.NavigationEndpoint.BrowseEndpoint.BrowseID
+	pageType = run.NavigationEndpoint.BrowseEndpoint.BrowseEndpointContextSupportedConfigs.BrowseEndpointContextMusicConfig.PageType
+	return
+}
+
+type NavigationEndpointParamsRuns []struct {
+	Text string
+
+	NavigationEndpoint struct {
+		BrowseEndpoint struct {
+			BrowseID string `json:"browseId,omitempty"`
+
+			Params string `json:"params,omitempty"`
+
+			BrowseEndpointContextSupportedConfigs struct {
+				BrowseEndpointContextMusicConfig struct {
+					PageType string `json:"pageType,omitempty"`
+				} `json:"browseEndpointContextMusicConfig,omitempty"`
+			} `json:"browseEndpointContextSupportedConfigs,omitempty"`
+		} `json:"browseEndpoint,omitempty"`
+	} `json:"navigationEndpoint,omitempty"`
+}
+
+func (runs *NavigationEndpointParamsRuns) GetTextField() string {
+	var str strings.Builder
+
+	if len((*runs)) > 0 {
+		for _, val := range *runs {
+			str.WriteString(val.Text)
+		}
+		return str.String()
+	}
+
+	return str.String()
+}
+
+func (runs *NavigationEndpointParamsRuns) GetFirstNavData() (pageType, browseId, browseParams string) {
+	if len((*runs)) == 0 {
+		return
+	}
+
+	run := (*runs)[0]
+	browseId = run.NavigationEndpoint.BrowseEndpoint.BrowseID
+	pageType = run.NavigationEndpoint.BrowseEndpoint.BrowseEndpointContextSupportedConfigs.BrowseEndpointContextMusicConfig.PageType
+	browseParams = run.NavigationEndpoint.BrowseEndpoint.Params
+	return
+}
+
+type NavigationAndWatchEndpointRuns struct {
+	BrowseEndpoint struct {
+		BrowseID string `json:"browseId,omitempty"`
+
+		BrowseEndpointContextSupportedConfigs struct {
+			BrowseEndpointContextMusicConfig struct {
+				PageType string `json:"pageType,omitempty"`
+			} `json:"browseEndpointContextMusicConfig,omitempty"`
+		} `json:"browseEndpointContextSupportedConfigs,omitempty"`
+	} `json:"browseEndpoint,omitempty"`
+
+	WatchEndpoint struct {
+		VideoID string `json:"videoId,omitempty"`
+
+		WatchEndpointMusicSupportedConfigs struct {
+			WatchEndpointMusicConfig struct {
+				MusicVideoType string `json:"musicVideoType,omitempty"`
+			} `json:"watchEndpointMusicConfig,omitempty"`
+		} `json:"watchEndpointMusicSupportedConfigs,omitempty"`
+	} `json:"watchEndpoint,omitempty"`
+}
