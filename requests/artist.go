591defe20e48caf807fcfd9ad578778685788383
Ritesh Ghosh <ghoshritesh55@gmail.com>
2024-06-01 19:07:01 +0530

    feat: add FetchArtist function to retrieve artist data from API

---
+++ requests/artist.go
@@ -0,0 +1,34 @@
+package requests
+
+import (
+	"net/url"
+
+	"github.com/ghoshRitesh12/brooktube/models/artist"
+	"github.com/ghoshRitesh12/brooktube/utils"
+)
+
+func FetchArtist(artistChannelID string) (*artist.APIResp, error) {
+	method := "POST"
+	reqURL, err := url.Parse(utils.HOST + utils.BROWSE_PATH)
+	if err != nil {
+		return nil, err
+	}
+
+	body := utils.NewBrowserEndpointContext(utils.MUSIC_PAGE_TYPE_ARTIST, artistChannelID)
+	queryParams := reqURL.Query()
+
+	queryParams.Set("prettyPrint", "false")
+	reqURL.RawQuery = queryParams.Encode()
+
+	headers := map[string]string{
+		"X-Goog-Visitor-Id":        utils.GOOG_VISITOR_ID,
+		"X-Youtube-Client-Version": utils.CLIENT_VERSION,
+	}
+
+	data, err := fetch[artist.APIResp](method, reqURL.String(), body, headers)
+	if err != nil {
+		return nil, err
+	}
+
+	return &data, nil
+}
