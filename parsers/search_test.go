287735ef6ea5da3725c7a12fc5383435a59caf72
Ritesh Ghosh <ghoshritesh55@gmail.com>
2024-06-01 19:02:44 +0530

    refactor: add subtests for GetSearchResults function in parsers package

--- parsers/search_test.go
+++ parsers/search_test.go
@@ -9,14 +9,102 @@ import (
 	"github.com/ghoshRitesh12/brooktube/parsers"
 )
 
-func TestGetSearchResults(t *testing.T) {
+func TestSubGetSearchSongs(t *testing.T) {
+	t.Parallel()
+	parser := brooktube.NewParser()
+
+	d, err := parser.GetSearchResults(
+		"black clover",
+		parsers.SearchParserParams{
+			Category: search.SONG_SEARCH_KEY,
+		},
+	)
+
+	for _, song := range d.Content.SongOrVideos {
+		spew.Dump(song)
+	}
+
+	spew.Dump(d.ContinuationToken)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestSubGetSearchVideos(t *testing.T) {
+	t.Parallel()
+	parser := brooktube.NewParser()
+
+	d, err := parser.GetSearchResults(
+		"black clover",
+		parsers.SearchParserParams{
+			Category: search.VIDEO_SEARCH_KEY,
+		},
+	)
+
+	for _, song := range d.Content.SongOrVideos {
+		spew.Dump(song)
+	}
+
+	spew.Dump(d.ContinuationToken)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestSubGetSearchArtists(t *testing.T) {
+	t.Parallel()
+	parser := brooktube.NewParser()
+
+	d, err := parser.GetSearchResults(
+		"black clover",
+		parsers.SearchParserParams{
+			Category: search.ARTIST_SEARCH_KEY,
+		},
+	)
+
+	for _, artist := range d.Content.Artists {
+		spew.Dump(artist)
+	}
+
+	spew.Dump(d.ContinuationToken)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestSubGetSearchAlbums(t *testing.T) {
+	t.Parallel()
+	parser := brooktube.NewParser()
+
+	d, err := parser.GetSearchResults(
+		"black clover",
+		parsers.SearchParserParams{
+			Category: search.ALBUM_SEARCH_KEY,
+		},
+	)
+
+	for _, album := range d.Content.Albums {
+		spew.Dump(album)
+	}
+
+	spew.Dump(d.ContinuationToken)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestSubGetSearchCommunityPlaylists(t *testing.T) {
+	t.Parallel()
 	parser := brooktube.NewParser()
 
 	d, err := parser.GetSearchResults(
 		"black clover",
 		parsers.SearchParserParams{
 			Category: search.COMMUNITY_PLAYLIST_SEARCH_KEY,
-			// ContinuationToken: "EqkDEgxibGFjayBjbG92ZXIamANFZ1dLQVFJSUFVZ1VhZ29RQ1JBRkVBb1FBeEFFZ2dFTE5qZGZaVkppVWpoWWFsV0NBUXRIUkd0MGVYQnpWbVk1V1lJQkMwVkZOak5sTWxKS2JIQmpnZ0VMVDFweWFEWlZORU41UTNlQ0FRdEZOMVZ1ZFc5dk1IWklRWUlCQzFKZmIzcEllbEJEWWxZMGdnRUxaRk56VG1neFp5MHlORldDQVF0NFFXdFRYeTFHVkVoSmM0SUJDM1pPWjFoWFUxQlJkMncwZ2dFTGFIVmpTVlZqZG1KQ2JVbUNBUXQ1U1d4Q1lqRktkbXBpYTRJQkMwMTFPSHBIUzJOWlgwTnJnZ0VMWDJRdE9FZ3RPRXhxWTFXQ0FRdHpYMkoxT1ZFd01ISTBRWUlCQzFoblMwa3hObDloWTJoVmdnRUxTMGRhU0UxTWRHTlJNVldDQVF0VFZVNUljMVowUkhSTWQ0SUJDekJNTjB0V1RYSXlZVkZuZ2dFTExUbHpNRXd0Y1Vad1JEaUNBUXRNVlRST2FVcFRRa05JUVElM0QlM0QY8erQLg%3D%3D",
 		},
 	)
 
