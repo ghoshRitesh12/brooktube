3868e93ec48cb47d128ccdf3349223e659be3e9f
Ritesh Ghosh <ghoshritesh55@gmail.com>
2024-06-01 19:02:25 +0530

    test: add unit test for GetArtist function in parsers package

---
+++ parsers/artist_test.go
@@ -0,0 +1,20 @@
+package parsers_test
+
+import (
+	"testing"
+
+	"github.com/davecgh/go-spew/spew"
+	"github.com/ghoshRitesh12/brooktube"
+)
+
+func TestGetArtist(t *testing.T) {
+	const EMINEM_CHANNEL_ID = "UCedvOgsKFzcK3hA5taf3KoQ"
+	parser := brooktube.NewParser()
+
+	artist, err := parser.GetArtist(EMINEM_CHANNEL_ID)
+	if err != nil {
+		t.Error(err)
+	}
+
+	spew.Dump(artist)
+}
