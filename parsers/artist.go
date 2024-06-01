11ed0a257285437893cb67aae61412efd959b91b
Ritesh Ghosh <ghoshritesh55@gmail.com>
2024-06-01 19:06:47 +0530

    feat: add GetArtist parser

---
+++ parsers/artist.go
@@ -0,0 +1,59 @@
+package parsers
+
+import (
+	"sync"
+
+	"github.com/ghoshRitesh12/brooktube/models/artist"
+	"github.com/ghoshRitesh12/brooktube/requests"
+)
+
+// const SCRAPE_OPERATIONS int = 6
+
+const SCRAPE_OPERATIONS int = 7
+
+func (p *YtMusicParser) GetArtist(artistChannelID string) (artist.ScrapedData, error) {
+	wg := &sync.WaitGroup{}
+	result := artist.ScrapedData{}
+
+	data, err := requests.FetchArtist(artistChannelID)
+	if err != nil {
+		return result, err
+	}
+
+	sections := &data.Contents.SingleColumnBrowseResultsRenderer.
+		Tabs[0].TabRenderer.Content.SectionListRenderer.Contents
+
+	if len(*sections) == 0 {
+		return result, nil
+	}
+
+	wg.Add(SCRAPE_OPERATIONS)
+
+	go result.ScrapeAndSetBasicInfo(wg, &data.Header, sections)
+	go result.SongsSection.ScrapeAndSet(wg, sections)
+
+	for _, section := range *sections {
+		sectionName := section.MusicCarouselShelfRenderer.
+			Header.MusicCarouselShelfBasicHeaderRenderer.
+			Title.Runs.GetTextField()
+
+		switch sectionName {
+		case "Albums":
+			go result.AlbumsSection.ScrapeAndSet(wg, &section)
+		case "Singles":
+			go result.SinglesSection.ScrapeAndSet(wg, &section)
+		case "Videos":
+			go result.VideosSection.ScrapeAndSet(wg, &section)
+		case "Featured on":
+			go result.FeaturedOnSection.ScrapeAndSet(wg, &section)
+		case "Fans might also like":
+			go result.AlikeArtistsSection.ScrapeAndSet(wg, &section)
+		default:
+			continue
+		}
+	}
+
+	wg.Wait()
+
+	return result, nil
+}
