42da58eaff1459643075f46be5b7ea78d54b8da3
Ritesh Ghosh <ghoshritesh55@gmail.com>
2024-06-04 20:22:15 +0530

    refactor: update artist section names to use constants

---
+++ models/artist/util.go
@@ -0,0 +1,11 @@
+package artist
+
+type SectionName string
+
+const (
+	SECTION_ALBUMS        SectionName = "Albums"
+	SECTION_SINGLES       SectionName = "Singles"
+	SECTION_VIDEOS        SectionName = "Videos"
+	SECTION_FEATURED_ON   SectionName = "Featured on"
+	SECTION_ALIKE_ARTISTS SectionName = "Fans might also like"
+)
