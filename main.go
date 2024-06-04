42594f9e0564d0e724c0e3afd51a792d7a7fc3d3
Ritesh Ghosh <ghoshritesh55@gmail.com>
2024-06-04 09:57:57 +0530

    refactor: update main.go to include ASCII art and rename NewParser to New

--- main.go
+++ main.go
@@ -4,6 +4,15 @@ import (
 	parser "github.com/ghoshRitesh12/brooktube/parsers"
 )
 
-func NewParser() *parser.YtMusicParser {
+const ASCIIArt string = `
+_                     _    _         _
+| |                   | |  | |       | |
+| |__  _ __ ___   ___ | | _| |_ _   _| |__   ___
+| '_ \| '__/ _ \ / _ \| |/ / __| | | | '_ \ / _ \
+| |_) | | | (_) | (_) |   <| |_| |_| | |_) |  __/
+|_.__/|_|  \___/ \___/|_|\_\\__|\__,_|_.__/ \___|
+`
+
+func New() *parser.YTMusicAPI {
 	return parser.New()
 }
