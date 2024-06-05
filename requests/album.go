8f2af67990187faeaf16116a4e1c4c947c4d83fb
Ritesh Ghosh <ghoshritesh55@gmail.com>
2024-06-05 14:22:50 +0530

    feat: add FetchAlbum function to retrieve album data

---
+++ requests/album.go
@@ -0,0 +1,35 @@
+package requests
+
+import (
+	"net/url"
+
+	"github.com/ghoshRitesh12/brooktube/helpers"
+	"github.com/ghoshRitesh12/brooktube/models/album"
+	"github.com/ghoshRitesh12/brooktube/utils"
+)
+
+func FetchAlbum(albumBrowseId string) (*album.APIResp, error) {
+	method := "POST"
+	reqURL, err := url.Parse(utils.HOST + utils.BROWSE_PATH)
+	if err != nil {
+		return nil, err
+	}
+
+	body := helpers.NewBrowserEndpointContext(utils.MUSIC_PAGE_TYPE_ALBUM, albumBrowseId)
+	queryParams := reqURL.Query()
+
+	queryParams.Set("prettyPrint", "false")
+	reqURL.RawQuery = queryParams.Encode()
+
+	headers := map[string]string{
+		"X-Goog-Visitor-Id":        utils.GOOG_VISITOR_ID,
+		"X-Youtube-Client-Version": utils.CLIENT_VERSION,
+	}
+
+	data, err := fetch[album.APIResp](method, reqURL.String(), body, headers)
+	if err != nil {
+		return nil, err
+	}
+
+	return &data, nil
+}
