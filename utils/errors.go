67e5ec5f1e90cfe301baf0006aced8067c07e2d3
Ritesh Ghosh <ghoshritesh55@gmail.com>
2024-06-05 14:20:08 +0530

    refactor: add errors for handling of album browse IDs

--- utils/errors.go
+++ utils/errors.go
@@ -4,4 +4,6 @@ import "errors"
 
 var (
 	ErrCouldntGetAlbumBrowseId = errors.New("could't get album browse id")
+	ErrInvalidAlbumBrowseId    = errors.New("invalid album browse id")
+	ErrAlbumContentsNotFound   = errors.New("album contents not found")
 )
