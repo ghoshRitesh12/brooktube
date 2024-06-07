4bec6f9b13f1d3ee948ddfddc8662d8489659804
Ritesh Ghosh <ghoshritesh55@gmail.com>
2024-06-07 23:27:45 +0530

    feat: add errors for handling of playlist IDs

--- utils/errors.go
+++ utils/errors.go
@@ -6,4 +6,12 @@ var (
 	ErrCouldntGetAlbumBrowseId = errors.New("could't get album browse id")
 	ErrInvalidAlbumBrowseId    = errors.New("invalid album browse id")
 	ErrAlbumContentsNotFound   = errors.New("album contents not found")
+	ErrInvalidAlbumId          = errors.New("invalid album id")
 )
+
+var (
+	ErrPlaylistContentsNotFound = errors.New("playlist contents not found")
+	ErrInvalidPlaylistId        = errors.New("invalid playlist id")
+)
+
+var ErrInvalidContinuationToken = errors.New("invalid continuation token")
