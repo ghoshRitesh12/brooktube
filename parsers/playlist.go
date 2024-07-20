1c3f0da8fc888e1e4d20573be9344399a0ac225c
Ritesh Ghosh <ghoshritesh55@gmail.com>
2024-07-20 20:20:28 +0530

    feat: update playlist parser and add continued content parser

--- parsers/playlist.go
+++ parsers/playlist.go
@@ -10,74 +10,95 @@ import (
 )
 
 const PLAYLIST_ID_PREFIX string = "VL"
-const (
-	PLAYLIST_SCRAPE_OPERATIONS        int = 2
-	PLAYLIST_SCRAPE_OPERATIONS_CTOKEN int = 1
-)
+const PLAYLIST_SCRAPE_OPERATIONS int = 2
 
-// `contiuationToken` is optional, if provided only 0th element will be used
-func (p *Scraper) GetPlaylist(
-	playlistId string,
-	continuationToken ...string,
-) (*playlist.ScrapedData, error) {
+func (p *Scraper) GetPlaylist(playlistId string) (*playlist.ScrapedData, error) {
 	if playlistId == "" {
 		return nil, utils.ErrInvalidPlaylistId
 	}
 
 	wg := &sync.WaitGroup{}
 	result := &playlist.ScrapedData{}
-	cToken := ""
 
-	if strings.HasPrefix(playlistId, PLAYLIST_ID_PREFIX) {
-		playlistId = playlistId[len(PLAYLIST_ID_PREFIX):]
-	} else {
+	if !strings.HasPrefix(playlistId, PLAYLIST_ID_PREFIX) {
 		playlistId = PLAYLIST_ID_PREFIX + playlistId
 	}
 
-	if len(continuationToken) > 0 {
-		cToken = continuationToken[0]
-		if cToken == "" {
-			return nil, utils.ErrInvalidContinuationToken
-		}
-	}
-
-	data, err := requests.FetchPlaylist(playlistId, cToken)
+	data, err := requests.FetchPlaylist(playlistId)
 	if err != nil {
 		return nil, err
 	}
 
-	// if ctoken is not provided, scrape basic info and tracks
-	if cToken == "" {
-		outerContents := data.Contents.
-			SingleColumnBrowseResultsRenderer.Tabs[0].
-			TabRenderer.Content.SectionListRenderer.Contents
-
-		if len(outerContents) < 1 {
-			return nil, utils.ErrPlaylistContentsNotFound
-		}
-
-		sections := &(outerContents[0].MusicPlaylistShelfRenderer.Contents)
-
-		if len(*sections) == 0 {
-			return result, nil
-		}
-
-		wg.Add(PLAYLIST_SCRAPE_OPERATIONS)
-
-		go result.ScrapeAndSetBasicInfo(wg, &data.Header)
-		go result.Tracks.ScrapeAndSet(wg, sections)
-
-		wg.Wait()
-
+	tabs := data.Contents.TwoColumnBrowseResultsRenderer.Tabs
+	if len(tabs) == 0 {
 		return result, nil
+	}
+
+	headerContents := tabs[0].TabRenderer.Content.SectionListRenderer.Contents
+	if len(headerContents) < 1 {
+		return nil, utils.ErrPlaylistContentsNotFound
+	}
+
+	outerContents := data.Contents.TwoColumnBrowseResultsRenderer.
+		SecondaryContents.SectionListRenderer.Contents
+	if len(outerContents) < 1 {
+		return nil, utils.ErrPlaylistContentsNotFound
+	}
+
+	sections := &(outerContents[0].MusicPlaylistShelfRenderer.Contents)
+
+	if len(*sections) == 0 {
+		return result, nil
+	}
+
+	result.ContinuationTokens = append(
+		result.ContinuationTokens,
+		outerContents[0].
+			MusicPlaylistShelfRenderer.
+			Continuations.GetContinuationToken(),
+		data.Contents.TwoColumnBrowseResultsRenderer.
+			SecondaryContents.SectionListRenderer.
+			Continuations.GetContinuationToken(),
+	)
+
+	wg.Add(PLAYLIST_SCRAPE_OPERATIONS)
+
+	go result.ScrapeAndSetBasicInfo(wg, &headerContents[0], &data.Background)
+	go result.Tracks.ScrapeAndSet(wg, sections)
+
+	wg.Wait()
+
+	return result, nil
+}
+
+// get more playlist tracks, if no more content is found, an empty array is returned
+func (p *Scraper) GetMorePlaylistTracks(playlistId, continuationToken string) (*playlist.Tracks, error) {
+	tracks := &playlist.Tracks{}
+
+	if playlistId == "" {
+		return nil, utils.ErrInvalidPlaylistId
+	}
+	if continuationToken == "" {
+		return nil, utils.ErrInvalidContinuationToken
+	}
+
+	if !strings.HasPrefix(playlistId, PLAYLIST_ID_PREFIX) {
+		playlistId = PLAYLIST_ID_PREFIX + playlistId
+	}
+
+	data, err := requests.FetchMorePlaylistTracks(playlistId, continuationToken)
+	if err != nil {
+		return nil, err
 	}
 
 	contents := &data.ContinuationContents.
 		MusicPlaylistShelfContinuation.Contents
 
-	wg.Add(PLAYLIST_SCRAPE_OPERATIONS_CTOKEN)
-	go result.Tracks.ScrapeAndSet(wg, contents)
-	wg.Wait()
+	if len(*contents) == 0 {
+		return tracks, nil
+	}
 
-	return result, nil
+	tracks.ScrapeAndSet(nil, contents)
+
+	return tracks, nil
 }
