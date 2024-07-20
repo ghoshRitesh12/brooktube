19bd1e36c0fa7652a3de6c4986a5fe894d81d17f
Ritesh Ghosh <ghoshritesh55@gmail.com>
2024-07-20 20:17:48 +0530

    test: improve get playlist test cases

--- parsers/playlist_test.go
+++ parsers/playlist_test.go
@@ -3,19 +3,48 @@ package parsers_test
 import (
 	"testing"
 
-	"github.com/davecgh/go-spew/spew"
 	"github.com/ghoshRitesh12/brooktube"
+	"github.com/stretchr/testify/assert"
 )
 
+// go test -run TestGetPlaylist ./parsers -v -count=1
 func TestGetPlaylist(t *testing.T) {
-	const NARUTO_OST_PLAYLIST = "VLPLF7A13C44809B5893"
-	// const BLACK_CLOVER_OST_PLAYLIST = "PLtwDCqqblBclwxAvPP0lTN56iWBjshBv7"
-	brooktube := brooktube.New()
-
-	playlist, err := brooktube.GetPlaylist(NARUTO_OST_PLAYLIST)
-	if err != nil {
-		t.Error(err)
+	testsTable := []struct {
+		name       string
+		playlistId string
+	}{
+		{"black clover playlist", "PLtwDCqqblBclwxAvPP0lTN56iWBjshBv7"},
+		{"naruto playlist", "VLPLF7A13C44809B5893"},
 	}
 
-	spew.Dump(playlist, len(playlist.Tracks))
+	btube := brooktube.New()
+
+	for i, test := range testsTable {
+		t.Run(test.name, func(t *testing.T) {
+			playlist, err := btube.GetPlaylist(test.playlistId)
+
+			assert.NoError(t, err)
+			assert.NotNil(t, playlist)
+
+			t.Log(playlist.CoverArt)
+
+			t.Run("continuation content", func(t *testing.T) {
+				if i == 0 {
+					t.Skip("skipping, as black clover playlist has no continuation tokens\n")
+				}
+				if len(playlist.ContinuationTokens) == 0 {
+					return
+				}
+
+				t.Logf("\nTracks count: %s\nTracks length: %d\n", playlist.TrackCount, len(playlist.Tracks))
+				t.Logf("\nPlaylist: %s\ncTokens: %#v\n\n", playlist.Title, playlist.ContinuationTokens)
+
+				tracks, err := btube.GetMorePlaylistTracks(test.playlistId, playlist.ContinuationTokens[0])
+				assert.NoError(t, err)
+				assert.NotEmpty(t, tracks)
+
+				// spew.Dump(tracks)
+			})
+		})
+	}
 }
