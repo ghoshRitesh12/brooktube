d7300b6848e6076f9486dad9e3b6b2ce604b3d3c
Ritesh Ghosh <ghoshritesh55@gmail.com>
2024-07-20 20:18:57 +0530

    feat: update playlist API response schema

--- models/playlist/api_response.go
+++ models/playlist/api_response.go
@@ -7,42 +7,38 @@ import (
 
 type APIResp struct {
 	Contents struct {
-		SingleColumnBrowseResultsRenderer struct {
-			Tabs []struct {
+		TwoColumnBrowseResultsRenderer struct {
+			SecondaryContents struct {
+				SectionListRenderer struct {
+					Contents [1]struct {
+						MusicPlaylistShelfRenderer apiRespSection `json:"musicPlaylistShelfRenderer,omitempty"`
+					} `json:"contents,omitempty"`
+
+					Continuations models.Continuations `json:"continuations,omitempty"`
+				} `json:"sectionListRenderer,omitempty"`
+			} `json:"secondaryContents,omitempty"`
+
+			Tabs [1]struct {
 				TabRenderer struct {
 					Content struct {
 						SectionListRenderer struct {
-							Contents [1]struct {
-								MusicPlaylistShelfRenderer apiRespSection `json:"musicPlaylistShelfRenderer,omitempty"`
-							} `json:"contents,omitempty"`
-
-							Continuations models.Continuations `json:"continuations,omitempty"`
+							Contents [1]apiRespHeader `json:"contents,omitempty"`
+							//
 						} `json:"sectionListRenderer,omitempty"`
 					} `json:"content,omitempty"`
 				} `json:"tabRenderer,omitempty"`
 			} `json:"tabs,omitempty"`
-		} `json:"singleColumnBrowseResultsRenderer,omitempty"`
+		} `json:"twoColumnBrowseResultsRenderer,omitempty"`
 	} `json:"contents,omitempty"`
 
-	// for meta data of the album
-	Header apiRespHeader `json:"header,omitempty"`
-
-	// for continuation data
-	ContinuationContents continuationContents `json:"continuationContents,omitempty"`
+	Background models.Thumbnail `json:"background,omitempty"`
 }
 
 type apiRespSection struct {
-	PlaylistId string `json:"playlistId,omitempty"`
-
-	Contents []apiRespSectionContent `json:"contents,omitempty"`
-
-	CollapsedItemCount int `json:"collapsedItemCount,omitempty"`
-}
-
-type continuationContents struct {
-	MusicPlaylistShelfContinuation struct {
-		Contents []apiRespSectionContent `json:"contents,omitempty"`
-	} `json:"musicPlaylistShelfContinuation,omitempty"`
+	Contents           []apiRespSectionContent `json:"contents,omitempty"`
+	PlaylistId         string                  `json:"playlistId,omitempty"`
+	CollapsedItemCount int                     `json:"collapsedItemCount,omitempty"`
+	Continuations      models.Continuations    `json:"continuations,omitempty"`
 }
 
 // for songs
@@ -65,26 +61,46 @@ type apiRespSectionContent struct {
 		} `json:"playlistItemData,omitempty"`
 
 		MusicItemRendererDisplayPolicy models.DisplayPolicy `json:"musicItemRendererDisplayPolicy,omitempty"`
-		//
+
+		Thumbnail models.Thumbnail `json:"thumbnail,omitempty"`
 	} `json:"musicResponsiveListItemRenderer,omitempty"`
 }
 
 type apiRespHeader struct {
-	MusicDetailHeaderRenderer struct {
+	MusicResponsiveHeaderRenderer struct {
 		Title struct {
 			Runs models.BasicRuns `json:"runs,omitempty"`
 		} `json:"title,omitempty"`
 
 		Subtitle struct {
-			Runs models.NavigationEndpointRuns `json:"runs,omitempty"`
+			Runs models.BasicRuns `json:"runs,omitempty"`
 		} `json:"subtitle,omitempty"`
 
+		StraplineTextOne struct {
+			Runs models.NavigationEndpointRuns `json:"runs,omitempty"`
+		} `json:"straplineTextOne,omitempty"`
+
 		Description struct {
-			Runs models.BasicRuns `json:"runs,omitempty"`
+			MusicDescriptionShelfRenderer struct {
+				Description struct {
+					Runs models.BasicRuns `json:"runs,omitempty"`
+				} `json:"description,omitempty"`
+			} `json:"musicDescriptionShelfRenderer,omitempty"`
 		} `json:"description,omitempty"`
 
 		SecondSubtitle struct {
 			Runs models.BasicRuns `json:"runs,omitempty"`
 		} `json:"secondSubtitle,omitempty"`
-	} `json:"musicDetailHeaderRenderer,omitempty"`
+	} `json:"musicResponsiveHeaderRenderer,omitempty"`
+}
+
+type APIRespContinuation struct {
+	// for continuation data
+	ContinuationContents continuationContents `json:"continuationContents,omitempty"`
+}
+
+type continuationContents struct {
+	MusicPlaylistShelfContinuation struct {
+		Contents []apiRespSectionContent `json:"contents,omitempty"`
+	} `json:"musicPlaylistShelfContinuation,omitempty"`
 }
