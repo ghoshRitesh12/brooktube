62581ba6c9836619b17f748c134500090d107553
Ritesh Ghosh <ghoshritesh55@gmail.com>
2024-07-20 20:17:02 +0530

    feat: improve code readability and add thumbnail object

--- models/common.go
+++ models/common.go
@@ -25,8 +25,7 @@ type Badges []struct {
 
 func (badges *Badges) IsExplicit() bool {
 	if len(*badges) > 0 {
-		return (*badges)[0].
-			MusicInlineBadgeRenderer.
+		return (*badges)[0].MusicInlineBadgeRenderer.
 			Icon.IconType == utils.MUSIC_EXPLICIT_BADGE
 	}
 	return false
@@ -37,4 +36,31 @@ type DisplayPolicy string
 func (displayPolicy DisplayPolicy) IsDisabled() bool {
 	policy := string(displayPolicy)
 	return policy == utils.MUSIC_ITEM_RENDERER_DISPLAY_POLICY_GREY_OUT || policy != ""
+}
+
+// for getting playlist and album thumbnail
+type Thumbnail struct {
+	MusicThumbnailRenderer struct {
+		Thumbnail struct {
+			Thumbnails []struct {
+				URL    string `json:"url,omitempty"`
+				Width  int    `json:"width,omitempty"`
+				Height int    `json:"height,omitempty"`
+			} `json:"thumbnails,omitempty"`
+		} `json:"thumbnail,omitempty"`
+	} `json:"musicThumbnailRenderer,omitempty"`
+}
+
+func (thmbnl *Thumbnail) GetThumbnail(index uint8) string {
+	url := ""
+	thumbnails := thmbnl.MusicThumbnailRenderer.Thumbnail.Thumbnails
+
+	if len(thumbnails) == 0 {
+		return url
+	}
+	if len(thumbnails) > 0 && int(index) < len(thumbnails) {
+		url = thumbnails[index].URL
+	}
+
+	return url
 }
