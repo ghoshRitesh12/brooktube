51483202d016e6d323969817afd717c21bf1bfcd
Ritesh Ghosh <ghoshritesh55@gmail.com>
2024-07-27 00:08:34 +0530

    test: add black clover playlist guy to artist's test cases

--- parsers/artist_test.go
+++ parsers/artist_test.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/davecgh/go-spew/spew"
 	"github.com/ghoshRitesh12/brooktube"
+	"github.com/stretchr/testify/assert"
 )
 
 // go test -run TestGetArtist ./parsers -v -count=1
@@ -15,19 +16,18 @@ func TestGetArtist(t *testing.T) {
 	}{
 		{"eminem", "UCedvOgsKFzcK3hA5taf3KoQ"},
 		{"naruto playlist guy", "UClX0RWY5bWVzGrWMTTD8iIQ"},
+		{"black clover playlist guy", "UCUup0eHG2-iKm7kVKqq0HzQ"},
 	}
 
 	btube := brooktube.New()
 
 	for _, test := range testsTable {
 		t.Run(test.channelName, func(t *testing.T) {
-
 			artist, err := btube.GetArtist(test.channelId)
-			if err != nil {
-				t.Error(err)
-			}
+			assert.NoError(t, err)
+			assert.NotNil(t, artist.Info)
 
-			spew.Dump(artist)
+			spew.Dump(artist.Info)
 		})
 	}
 }
