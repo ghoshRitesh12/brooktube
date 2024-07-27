aa67f867a9e1191eaaefd0f4ec70502d3245cf18
Ritesh Ghosh <ghoshritesh55@gmail.com>
2024-07-27 23:44:26 +0530

    test: add new search test params

--- parsers/search_test.go
+++ parsers/search_test.go
@@ -6,115 +6,126 @@ import (
 	"github.com/davecgh/go-spew/spew"
 	"github.com/ghoshRitesh12/brooktube"
 	"github.com/ghoshRitesh12/brooktube/internal/models/search"
-	"github.com/ghoshRitesh12/brooktube/parsers"
+	"github.com/stretchr/testify/assert"
 )
 
-func TestSubGetSearchSongs(t *testing.T) {
-	t.Parallel()
-	brooktube := brooktube.New()
+// go test -run TestGetSearchResults ./parsers -v -count=1
+func TestGetSearchResults(t *testing.T) {
+	innertube := brooktube.New()
 
-	d, err := brooktube.GetSearchResults(
-		"black clover",
-		parsers.SearchParserParams{
-			Category: search.SONG_SEARCH_KEY,
-		},
-	)
-
-	for _, song := range d.Content.Songs {
-		spew.Dump(song)
+	testsTable := []struct {
+		query    string
+		category search.SearchCategory
+	}{
+		{"black clover", search.SONG_SEARCH_KEY},
+		// {"black clover", search.VIDEO_SEARCH_KEY},
 	}
 
-	spew.Dump(d.ContinuationToken)
+	for _, test := range testsTable {
+		t.Run(test.query, func(t *testing.T) {
+			// results, err := innertube.GetSearchResults(test.query, test.category)
+			result, err := innertube.Search.GetVideoResults(test.query)
 
-	if err != nil {
-		t.Fatal(err)
-	}
-}
+			assert.NoError(t, err)
+			assert.NotEmpty(t, result.Contents)
+			spew.Dump(result)
 
-func TestSubGetSearchVideos(t *testing.T) {
-	t.Parallel()
-	brooktube := brooktube.New()
+			t.Run("continuation", func(t *testing.T) {
+				t.Parallel()
+				res, err := innertube.Search.GetNextVideoResults(test.query, result.Info.ContinuationToken)
 
-	d, err := brooktube.GetSearchResults(
-		"black clover",
-		parsers.SearchParserParams{
-			Category: search.VIDEO_SEARCH_KEY,
-		},
-	)
-
-	for _, video := range d.Content.Videos {
-		spew.Dump(video)
-	}
-
-	spew.Dump(d.ContinuationToken)
-
-	if err != nil {
-		t.Fatal(err)
+				assert.NoError(t, err)
+				assert.NotEmpty(t, res.Contents)
+				spew.Dump(res)
+			})
+		})
 	}
 }
 
-func TestSubGetSearchArtists(t *testing.T) {
-	t.Parallel()
-	brooktube := brooktube.New()
+// func TestSubGetSearchVideos(t *testing.T) {
+// 	t.Parallel()
+// 	brooktube := brooktube.New()
 
-	d, err := brooktube.GetSearchResults(
-		"black clover",
-		parsers.SearchParserParams{
-			Category: search.ARTIST_SEARCH_KEY,
-		},
-	)
+// 	d, err := brooktube.GetSearchResults(
+// 		"black clover",
+// 		parsers.SearchParserParams{
+// 			Category: search.VIDEO_SEARCH_KEY,
+// 		},
+// 	)
 
-	for _, artist := range d.Content.Artists {
-		spew.Dump(artist)
-	}
+// 	for _, video := range d.Content.Videos {
+// 		spew.Dump(video)
+// 	}
 
-	spew.Dump(d.ContinuationToken)
+// 	spew.Dump(d.ContinuationToken)
 
-	if err != nil {
-		t.Fatal(err)
-	}
-}
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
+// }
 
-func TestSubGetSearchAlbums(t *testing.T) {
-	t.Parallel()
-	brooktube := brooktube.New()
+// func TestSubGetSearchArtists(t *testing.T) {
+// 	t.Parallel()
+// 	brooktube := brooktube.New()
 
-	d, err := brooktube.GetSearchResults(
-		"black clover",
-		parsers.SearchParserParams{
-			Category: search.ALBUM_SEARCH_KEY,
-		},
-	)
+// 	d, err := brooktube.GetSearchResults(
+// 		"black clover",
+// 		parsers.SearchParserParams{
+// 			Category: search.ARTIST_SEARCH_KEY,
+// 		},
+// 	)
 
-	for _, album := range d.Content.Albums {
-		spew.Dump(album)
-	}
+// 	for _, artist := range d.Content.Artists {
+// 		spew.Dump(artist)
+// 	}
 
-	spew.Dump(d.ContinuationToken)
+// 	spew.Dump(d.ContinuationToken)
 
-	if err != nil {
-		t.Fatal(err)
-	}
-}
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
+// }
 
-func TestSubGetSearchCommunityPlaylists(t *testing.T) {
-	t.Parallel()
-	brooktube := brooktube.New()
+// func TestSubGetSearchAlbums(t *testing.T) {
+// 	t.Parallel()
+// 	brooktube := brooktube.New()
 
-	d, err := brooktube.GetSearchResults(
-		"black clover",
-		parsers.SearchParserParams{
-			Category: search.COMMUNITY_PLAYLIST_SEARCH_KEY,
-		},
-	)
+// 	d, err := brooktube.GetSearchResults(
+// 		"black clover",
+// 		parsers.SearchParserParams{
+// 			Category: search.ALBUM_SEARCH_KEY,
+// 		},
+// 	)
 
-	for _, cmPlaylist := range d.Content.CommunityPlaylists {
-		spew.Dump(cmPlaylist)
-	}
+// 	for _, album := range d.Content.Albums {
+// 		spew.Dump(album)
+// 	}
 
-	spew.Dump(d.ContinuationToken)
+// 	spew.Dump(d.ContinuationToken)
 
-	if err != nil {
-		t.Fatal(err)
-	}
-}
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
+// }
+
+// func TestSubGetSearchCommunityPlaylists(t *testing.T) {
+// 	t.Parallel()
+// 	brooktube := brooktube.New()
+
+// 	d, err := brooktube.GetSearchResults(
+// 		"black clover",
+// 		parsers.SearchParserParams{
+// 			Category: search.COMMUNITY_PLAYLIST_SEARCH_KEY,
+// 		},
+// 	)
+
+// 	for _, cmPlaylist := range d.Content.CommunityPlaylists {
+// 		spew.Dump(cmPlaylist)
+// 	}
+
+// 	spew.Dump(d.ContinuationToken)
+
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
+// }
