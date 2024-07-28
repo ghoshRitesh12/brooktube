bb7109e2bedef8e96b0d15cbe938515d6ab49e42
Ritesh Ghosh <ghoshritesh55@gmail.com>
2024-07-28 10:46:25 +0530

    refactor: remove ascii art and update scraper imoprt path

--- main.go
+++ main.go
@@ -1,15 +1,6 @@
 package brooktube
 
-import "github.com/ghoshRitesh12/brooktube/parsers"
-
-const ASCIIArt string = `
- _                     _    _         _
-| |                   | |  | |       | |
-| |__  _ __ ___   ___ | | _| |_ _   _| |__   ___
-| '_ \| '__/ _ \ / _ \| |/ / __| | | | '_ \ / _ \
-| |_) | | | (_) | (_) |   <| |_| |_| | |_) |  __/
-|_.__/|_|  \___/ \___/|_|\_\\__|\__,_|_.__/ \___|
-`
+import "github.com/ghoshRitesh12/brooktube/internal/parsers"
 
 type scraper struct {
 	parsers.Scraper
