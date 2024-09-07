937f0cbe84abd8159e340b6bac24906bead5a1ad
Ritesh Ghosh <ghoshritesh55@gmail.com>
2024-09-08 00:32:54 +0530

    feat: add ErrInvalidMediaId, ErrLyricsContentNotFound and ErrLyricsNotFound

--- internal/errors/errors.go
+++ internal/errors/errors.go
@@ -9,12 +9,19 @@ var (
 	ErrCouldntGetAlbumBrowseId = errors.New("album_parser: could't get album browse id")
 )
 
+var ErrInvalidMediaId = errors.New("brooktube: invalid song or video id")
+
 var (
 	ErrInvalidPlaylistId        = errors.New("playlist_parser: invalid playlist id")
 	ErrPlaylistContentsNotFound = errors.New("playlist_parser: playlist contents not found")
 )
 
 var ErrArtistContentNotFound = errors.New("artist_parser: artist content not found")
+
+var (
+	ErrLyricsContentNotFound = errors.New("lyrics_parser: lyrics content not found")
+	ErrLyricsNotFound        = errors.New("lyrics_parser: lyrics unavailable for this song")
+)
 
 var ErrInvalidContinuationToken = errors.New("continued_content: invalid continuation token")
 
