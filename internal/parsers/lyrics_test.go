bde43be6292665329773f41e4443615d6ef04dac
Ritesh Ghosh <ghoshritesh55@gmail.com>
2024-09-08 00:33:37 +0530

    feat: add unit test for GetLyrics function in parsers package

---
+++ internal/parsers/lyrics_test.go
@@ -0,0 +1,33 @@
+package parsers_test
+
+import (
+	"testing"
+
+	"github.com/davecgh/go-spew/spew"
+	"github.com/ghoshRitesh12/brooktube"
+	"github.com/stretchr/testify/assert"
+)
+
+// go test -run TestGetLyrics ./internal/parsers -v -count=1
+func TestGetLyrics(t *testing.T) {
+	testsTable := []struct {
+		mediaName string
+		mediaId   string
+	}{
+		{"borderline by tape impala", "2g5xkLqIElU"},  // should pass
+		{"yellow by yoh kamiyama", "waYhTInn7GU"},     // should pass
+		{"renegade by jayz ft eminem", "3JTLkJVDDE4"}, // should fail
+	}
+
+	btube := brooktube.New()
+
+	for _, test := range testsTable {
+		t.Run(test.mediaName, func(t *testing.T) {
+			lyricsData, err := btube.GetLyrics(test.mediaId)
+			assert.NoError(t, err)
+			assert.NotEmpty(t, lyricsData)
+
+			spew.Dump(lyricsData)
+		})
+	}
+}
