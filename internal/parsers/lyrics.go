a8fdf092b7d47b2fb452829fc8a8d6dd84caefc0
Ritesh Ghosh <ghoshritesh55@gmail.com>
2024-09-19 11:17:19 +0530

    refactor: update btube error package import in lyrics.go

--- internal/parsers/lyrics.go
+++ internal/parsers/lyrics.go
@@ -3,7 +3,7 @@ package parsers
 import (
 	"errors"
 
-	btubeErrs "github.com/ghoshRitesh12/brooktube/internal/errors"
+	btube_errs "github.com/ghoshRitesh12/brooktube/internal/errors"
 	"github.com/ghoshRitesh12/brooktube/internal/models/lyrics"
 	"github.com/ghoshRitesh12/brooktube/internal/requests"
 )
@@ -11,14 +11,14 @@ import (
 // {mediaId} refers to video or song id
 func (p *Scraper) GetLyrics(mediaId string) (*lyrics.ScrapedData, error) {
 	if mediaId == "" {
-		return nil, btubeErrs.ErrInvalidMediaId
+		return nil, btube_errs.ErrInvalidMediaId
 	}
 
 	result := &lyrics.ScrapedData{}
 
 	data, err := requests.FetchLyricsData(mediaId)
-	if err != nil {
-		if errors.Is(err, btubeErrs.ErrLyricsNotFound) {
+	if err != nil || data == nil {
+		if errors.Is(err, btube_errs.ErrLyricsNotFound) {
 			result.Lyrics = "Lyrics unavailable for this song"
 			return result, nil
 		}
