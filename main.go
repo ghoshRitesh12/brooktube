ee8c8a769ad0a1b7914bbd91fcf297655d676eff
Ritesh Ghosh <ghoshritesh55@gmail.com>
2024-09-19 11:19:39 +0530

    feat: add brooktube error exports for consumer use cases

--- main.go
+++ main.go
@@ -1,12 +1,31 @@
 package brooktube
 
-import "github.com/ghoshRitesh12/brooktube/internal/parsers"
+import (
+	"github.com/ghoshRitesh12/brooktube/internal/errors"
+	"github.com/ghoshRitesh12/brooktube/internal/parsers"
+)
+
+var (
+	ErrInvalidAlbumId           = errors.ErrInvalidAlbumId
+	ErrInvalidAlbumBrowseId     = errors.ErrInvalidAlbumBrowseId
+	ErrAlbumContentsNotFound    = errors.ErrAlbumContentsNotFound
+	ErrCouldntGetAlbumBrowseId  = errors.ErrCouldntGetAlbumBrowseId
+	ErrInvalidMediaId           = errors.ErrInvalidMediaId
+	ErrInvalidPlaylistId        = errors.ErrInvalidPlaylistId
+	ErrPlaylistContentsNotFound = errors.ErrPlaylistContentsNotFound
+	ErrArtistContentNotFound    = errors.ErrArtistContentNotFound
+	ErrLyricsContentNotFound    = errors.ErrLyricsContentNotFound
+	ErrLyricsNotFound           = errors.ErrLyricsNotFound
+	ErrInvalidContinuationToken = errors.ErrInvalidContinuationToken
+	ErrSearchResultsNotFound    = errors.ErrSearchResultsNotFound
+	ErrInvalidSearchCategory    = errors.ErrInvalidSearchCategory
+)
 
 type scraper struct {
 	parsers.Scraper
 }
 
-// initialize a new scraper
+// initialize a new [scraper] instance
 func New() *scraper {
 	return &scraper{}
 }
